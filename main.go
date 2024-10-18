b3bb1d4d4853ccc8c7b16a804b40b00ba1f14524
Nickolay Kirilman <nikolajkirilman@gmail.com>
2024-10-19 00:45:02 +0300

    Complete

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main(){
-	
+	a := [4]int{1,2,3,4}
+	reverse(&a)
+	fmt.Println(a)
+}
+
+func reverse(arr *[4]int){
+	for index, value := range *arr {
+		(*arr)[len(arr)-1-index] =  value
+
+	}
 }
