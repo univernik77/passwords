b449f0b5a27f56867272e830829a40c2077dc15f
Nickolay Kirilman <nikolajkirilman@gmail.com>
2024-10-20 12:14:54 +0300

    Add method

--- main.go
+++ main.go
@@ -1,16 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
+       
+
+type account struct {
+    login string
+    password string
+    url string
+}
+
+func (acc *account) outputPassword() {
+    fmt.Println(acc.login, acc.password, acc.url)
+
+}
+
+func (acc *account) generatePassword(n int) {
+    password := make([]rune, n)
+    for i  := range password {
+        password[i] = letterRunes[rand.IntN(len(letterRunes))]
+    }
+    acc.password = string(password)
+}
+
+var letterRunes = []rune("asdfghjklzxcvbnmqwertyuiopASDFGHJKLZXCVBNMQWERTYUIOP")
 
 func main(){
-	a := [4]int{1,2,3,4}
-	reverse(&a)
-	fmt.Println(a)
+    login := promtData("Введите логин:")
+    //password := promtData("Введите пароль:")
+    url := promtData("Введите URL:")
+    
+    myAccount := account{
+        login: login,
+        //password: password,
+        url: url,
+    }
+
+    myAccount.generatePassword(12)
+    myAccount.outputPassword()
+	
 }
 
-func reverse(arr *[4]int){
-	for index, value := range *arr {
-		(*arr)[len(arr)-1-index] =  value
-
-	}
+func promtData(promt string) string {
+    fmt.Print(promt + " ")
+    var res string
+    fmt.Scan(&res)
+    return res
 }
+
+
+
